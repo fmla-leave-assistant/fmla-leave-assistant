796bd43a45d603b76138edd23627818a16c4a216
Adrienne <adrienneeaston@gmail.com>
2019-05-23 21:46:11 -0700

    constructer, schema, and function in progress

--- server.js
+++ server.js
@@ -34,4 +34,34 @@ function handleError(err, response) {
   if (response) response.status(500).send('Bruh, something didn\'t work');
 }
 
+function getSpreadSheet(request, response) {
+  let url= 'https://sheets.googleapis.com/v4/spreadsheets/1xTi2w8NV6QqRjoZDyMrfwbSpBjjakBFJrIpPkCZ5UgI/values/Sheet1?valueRenderOption=FORMATTED_VALUE&key=AIzaSyA0vxlhkfqH7U7BdUtnGb3zWiTMbcJrg0U'
+
+  superagent.get(url) 
+  .then(results => {
+    let data = results.body.values
+    let parsedRows = data.map(row => {
+      Row(row)
+    })
+    parsedRows.shift();
+    parsedRows.shift();
+    })
+    .then(refinedData => console.log(refinedData))
+    .catch(error => handleError(error, reponse));
+};
+
+getSpreadSheet();
+
+function Row(info) {
+  this.bossColumn = info[0];
+  this.nameColumn = info[1];
+  this.badgeColumn = info[2];
+  this.sick_leaveColumn = info[6];
+  this.rdo = info[4];
+  this.first = info[7];
+  this.second = info[9];
+}
+
+
+
 
