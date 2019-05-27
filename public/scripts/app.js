ac5b02ab9c10a72d5604ca73629a566e5d432b5f
leeroywking <leeroywking@gmail.com>
2019-05-27 11:30:29 -0700

    cleaned it up and made it a prototype of Date

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,10 @@ function flipLogin () {
     $('#createuser, #loginform').toggle()
 }
 
+// Date.prototype.getDayOfYear() = {Math.floor((((new Date()) - (new Date(new Date().getFullYear(), 0, 0))) / 525600/ 60000 )* 365)}
+// const getDayOfYear = () => Math.floor((((new Date()) - (new Date(new Date().getFullYear(), 0, 0))) / 525600/ 60000 )* 365)
 
-const getDayOfYear = () => Math.floor((((new Date()) - (new Date(new Date().getFullYear(), 0, 0))) / 525600/ 60000 )* 365)
+Date.prototype.getDayNumber = () => Math.floor((((new Date()) - (new Date(new Date().getFullYear(), 0, 0))) / 525600/ 60000 )* 365)
 
-$('#replacewithdayofyear')[0].defaultValue = getDayOfYear()
+
+$('#replacewithdayofyear')[0].defaultValue = new Date().getDayNumber()
