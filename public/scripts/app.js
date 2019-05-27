ec9a901fb4f3b38c942e271e98afb6fd73482e37
leeroywking <leeroywking@gmail.com>
2019-05-27 10:55:46 -0700

    added in the day function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,3 +5,6 @@ $('#createuser').toggle()
 function flipLogin () {
     $('#createuser, #loginform').toggle()
 }
+
+
+const getDayOfYear = () => Math.floor((((new Date()) - (new Date(now.getFullYear(), 0, 0))) / 525600/ 60000 )* 365)
